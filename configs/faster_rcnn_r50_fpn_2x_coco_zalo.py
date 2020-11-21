ceedcfc378486085ddde15ea3b80570090a34684
huylb314 <huylb314@gmail.com>
2020-11-22 02:48:00 +0700

    modify relative path

--- configs/faster_rcnn_r50_fpn_2x_coco_zalo.py
+++ configs/faster_rcnn_r50_fpn_2x_coco_zalo.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/faster_rcnn_r50_fpn_zalo.py',
-    '../_base_/datasets/zalo_detection.py',
-    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+    './_base_/models/faster_rcnn_r50_fpn_zalo.py',
+    './_base_/datasets/zalo_detection.py',
+    './_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
 ]
