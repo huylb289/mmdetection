e12d68ace87969fa0842ea20e7e1d88205266ee7
huylb314 <huylb314@gmail.com>
2020-11-22 03:20:52 +0700

    remove colab

--- configs/faster_rcnn_r50_fpn_2x_coco_zalo_colab.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    './_base_/models/faster_rcnn_r50_fpn_zalo.py',
-    './_base_/datasets/zalo_detection.py',
-    './_base_/schedules/schedule_2x.py', './_base_/default_runtime_colab.py'
-]
